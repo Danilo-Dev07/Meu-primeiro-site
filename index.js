b2ca7542c07be12e72652693e07e93d7ca4bf9c5
Danilo <danilov.silva2007@gmail.com>
2026-03-09 09:23:51 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-
 'use strict';
+// Source - https://stackoverflow.com/q/32405812
+// Posted by user2133925
+// Retrieved 2026-03-09, License - CC BY-SA 3.0
 
 document.addEventListener('DOMContentLoaded', () => {
     const switcher = document.querySelector('.btn');
